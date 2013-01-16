47cbd599e1a27c9bb3500007d5a3110a44049317
ZorroGuo <guoshaoqing1987@126.com>
2013-01-16 23:10:47 +0800

    MyHome Project Tools Dev Level

---
+++ src/cn/zorroguo/myhome/tools/UtilTools.java
@@ -0,0 +1,22 @@
+package cn.zorroguo.myhome.tools;
+
+/**
+ * UtilTools.java
+ * <p>
+ * Created on 2013-1-16
+ * Modification history
+ * <p>
+ * @author   Shaoqing Guo,guoshaoqing1987@126.com
+ * @version  1.0
+ * @since    1.0
+ */
+public class UtilTools {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi, GitHub!");
+	}
+
+}
